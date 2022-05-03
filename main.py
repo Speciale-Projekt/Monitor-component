7b79c4d665c40fe5b33c6f2aed4d123dfd552a3e
Lars Bo Frydenskov <lb@innobeat.dk>
2022-05-03 10:35:33 +0200

    Back to inotify and threads

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ import threading
 # Then we should send the content via UDP to the server
 ip = "127.0.0.1"
 
-filename = pathlib.Path("/home/lars/test/out.txt")
+filename = pathlib.Path("/home/lars/test/device.bin")
 sm = ThreadStateMachine(Thread.states[0])
+
 
 def proxy(listenport, servport):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,7 +26,35 @@ def proxy(listenport, servport):
         print(data)
         sock.sendto(data, (ip, servport))
 
-
+def superduper(port):
+    i = inotify.adapters.Inotify()
+    if not os.path.isfile(filename):
+        with open(filename, "w+") as f:
+            pass
+    # open the file and read the content
+    i.add_watch(filename.__str__())
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, path, _) = event
+        print(event)
+        if "IN_CLOSE_WRITE" in type_names or "IN_MOVE_SELF" in type_names or "IN_MODIFY" in type_names:
+            # Read file, its in binary format
+            with open(filename, "rb") as f:
+                sock = socket.socket(socket.AF_INET,
+                                     socket.SOCK_DGRAM)
+                content = f.read()
+                print(content)
+                sock.sendto(content, (ip, port))
+                print("File sent to server")
+        else:
+            # check what files are in notify watchlist
+            try:
+                i.remove_watch(filename.__str__())
+            except Exception as ignored:
+                pass
+            i.add_watch(filename.__str__())
+            if not os.path.isfile(filename):
+                with open(filename, "w+") as f:
+                    pass  # remove old watch, and add new watch to same file
 
 def _main():
     if len(sys.argv) == 1:
@@ -42,38 +71,11 @@ def _main():
     print("Serv port is: ", servport)
     print("Aflnet local port is:", port)
 
-    thread = threading.Thread(target=proxy, args=(listenport,servport))
+    thread = threading.Thread(target=superduper, args=(port,))
+    #thread = threading.Thread(target=proxy, args=(listenport, servport))
     thread.start()
+    proxy(listenport, servport)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    i = inotify.adapters.Inotify()
-    if not os.path.isfile(filename):
-        with open(filename, "w+") as f:
-            pass
-    # open the file and read the content
-    i.add_watch(filename.__str__())
-    for event in i.event_gen(yield_nones=False):
-       (_, type_names, path, _) = event
-       print(event)
-       if "IN_CLOSE_WRITE" in type_names or "IN_MOVE_SELF" in type_names or "IN_MODIFY" in type_names:
-           # Read file, its in binary format
-           with open(filename, "rb") as f:
-               sock = socket.socket(socket.AF_INET,
-                                    socket.SOCK_DGRAM)
-               content = f.read()
-               print(content)
-               sock.sendto(content, (ip, port))
-               print("File sent to server")
-       else:
-           # check what files are in notify watchlist
-           try:
-               i.remove_watch(filename.__str__())
-           except Exception as ignored:
-               pass
-           i.add_watch(filename.__str__())
-           if not os.path.isfile(filename):
-               with open(filename, "w+") as f:
-                   pass  # remove old watch, and add new watch to same file
 
 
 if __name__ == '__main__':
