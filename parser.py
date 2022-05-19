5090bd3e5951995a97d02ccaeaa5b016217008a4
Asger Geel Weirsøe <asger@weirsoe.dk>
2022-05-19 11:17:34 +0200

    updates parser to work like it does in the parsepackage

--- parser.py
+++ parser.py
@@ -1,15 +1,7 @@
 import pathlib
 
-# MAGIC VARIABLES
 
-child_addr = b"fe80:0:0:0:588a"
-master_addr = b"fe80:0:0:0:243a"
-multi_cast = b"ff02:0:0:0:0:0:"
-child_determiner = b'\x43\x48\x49\x4C\x44'
-master_determiner = b'\x4D\x41\x53\x54\x45\x52'
-
-
-def read_file(file_name: pathlib.Path) -> str:
+def read_file(file_name: pathlib.Path) -> bin:
     """Open file as binary and return the data."""
     with open(file_name, "rb") as file:
         data = file.read()
@@ -21,109 +13,89 @@ def print_hex(data: bin) -> str:
     return " ".join(["{:02X}".format(x) for x in data])
 
 
-def handle_messages(data: bin, determ: bin) -> list:
-    """
-    Parse the data and return a dictionary with the messages.
-    :param data: binary data to parse
-    :param determ: determinator to look for new messages
-    :return: list of dictionary with the following: {count: int, destination: bin, message: bin}
-    """
-    messages = []
-    for i in range(len(data)):
-        if data[i:i + len(determ)] == determ:
-            q = j = count = 0
-            dest_addr = None
-
-            for j in range(i + len(determ) + 1, len(data)):
-                if data[j:j + 1] == b'\x3A':
-                    count = int(data[i + len(determ) + 1:j])
-                    break
-            for q in range(j + 1, len(data)):
-                if data[q:q + 1] == b'\x5B':
-                    dest_addr = data[j + 1:q]
-                    break
-
-            for o in range(q + 1, len(data)):
-                if data[o:o + 1] == b'\x5D':
-                    messages.append({
-                        "destination": dest_addr,
-                        "count": count,
-                        "message": data[q + 1:o]
-                    })
-                    break
-    return messages
-
-
-def assign_command_type(messages: bin) -> dict:
+def assign_command_type(message: bin, res=None) -> list:
     """
     Assign the command type and the TLV names to the message.
-    :param messages: binary data
-    :return: the same dictionary, but with command_type and tlvs
+    :param message:
+    :return: command and tlvs
     """
-    for message in messages:
-        if message == {}:
-            continue
-        if message.get("message")[0:1] == b'\xFF':
-            command_type = message.get("message")[1:2]
-            message["tlvs"] = parse_message(message.get("message")[2:])
-        else:
-            command_type = message.get("message")[11:12]
-            message["tlvs"] = parse_message(message.get("message")[12:])
-        if command_type == b'\x00':
-            message["command_type"] = {"command_index": 0, "name": "Link Request"}
-        elif command_type == b'\x01':
-            message["command_type"] = {"command_index": 1, "name": "Link Accept"}
-        elif command_type == b'\x02':
-            message["command_type"] = {"command_index": 2, "name": "Link Accept and Request"}
-        elif command_type == b'\x03':
-            message["command_type"] = {"command_index": 3, "name": "Link Reject"}
-        elif command_type == b'\x04':
-            message["command_type"] = {"command_index": 4, "name": "Advertisement"}
-        elif command_type == b'\x05':
-            message["command_type"] = {"command_index": 5, "name": "Update"}
-        elif command_type == b'\x06':
-            message["command_type"] = {"command_index": 6, "name": "Update Request"}
-        elif command_type == b'\x07':
-            message["command_type"] = {"command_index": 7, "name": "Data Request"}
-        elif command_type == b'\x08':
-            message["command_type"] = {"command_index": 8, "name": "Data Response"}
-        elif command_type == b'\x09':
-            message["command_type"] = {"command_index": 9, "name": "Parent Request"}
-        elif command_type == b'\x0A':
-            message["command_type"] = {"command_index": 10, "name": "Parent Response"}
-        elif command_type == b'\x0B':
-            message["command_type"] = {"command_index": 11, "name": "Child ID Request"}
-        elif command_type == b'\x0C':
-            message["command_type"] = {"command_index": 12, "name": "Child ID Response"}
-        elif command_type == b'\x0D':
-            message["command_type"] = {"command_index": 13, "name": "Child Update Request"}
-        elif command_type == b'\x0E':
-            message["command_type"] = {"command_index": 14, "name": "Child Update Response"}
-        elif command_type == b'\x0F':
-            message["command_type"] = {"command_index": 15, "name": "Announce"}
-        elif command_type == b'\x10':
-            message["command_type"] = {"command_index": 16, "name": "Discovery Request"}
-        elif command_type == b'\x11':
-            message["command_type"] = {"command_index": 17, "name": "Discovery Response"}
-        else:
-            message["command_type"] = {"command_index": -1, "name": "Unknown command type"}
-    return messages
+
+    if res is None:
+        res = []
+    if message == b'':
+        return res
+    if message[0:1] == b'\xFF':
+        command_type = message[1:2]
+        next_message_index, tlvs = get_tlvs(message[2:])
+        if next_message_index is not None:
+            next_message_index += 2
+    else:
+        command_type = message[11:12]
+        next_message_index, tlvs = get_tlvs(message[12:])
+        if next_message_index is not None:
+            next_message_index += 12
+    if command_type == b'\x00':
+        command = {"command_index": 0, "name": "Link Request"}
+    elif command_type == b'\x01':
+        command = {"command_index": 1, "name": "Link Accept"}
+    elif command_type == b'\x02':
+        command = {"command_index": 2, "name": "Link Accept and Request"}
+    elif command_type == b'\x03':
+        command = {"command_index": 3, "name": "Link Reject"}
+    elif command_type == b'\x04':
+        command = {"command_index": 4, "name": "Advertisement"}
+    elif command_type == b'\x05':
+        command = {"command_index": 5, "name": "Update"}
+    elif command_type == b'\x06':
+        command = {"command_index": 6, "name": "Update Request"}
+    elif command_type == b'\x07':
+        command = {"command_index": 7, "name": "Data Request"}
+    elif command_type == b'\x08':
+        command = {"command_index": 8, "name": "Data Response"}
+    elif command_type == b'\x09':
+        command = {"command_index": 9, "name": "Parent Request"}
+    elif command_type == b'\x0A':
+        command = {"command_index": 10, "name": "Parent Response"}
+    elif command_type == b'\x0B':
+        command = {"command_index": 11, "name": "Child ID Request"}
+    elif command_type == b'\x0C':
+        command = {"command_index": 12, "name": "Child ID Response"}
+    elif command_type == b'\x0D':
+        command = {"command_index": 13, "name": "Child Update Request"}
+    elif command_type == b'\x0E':
+        command = {"command_index": 14, "name": "Child Update Response"}
+    elif command_type == b'\x0F':
+        command = {"command_index": 15, "name": "Announce"}
+    elif command_type == b'\x10':
+        command = {"command_index": 16, "name": "Discovery Request"}
+    elif command_type == b'\x11':
+        command = {"command_index": 17, "name": "Discovery Response"}
+    else:
+        command = {"command_index": -1, "name": "Unknown command type"}
+    res.append({**command, **{"tlvs": tlvs}, **{"total_message": print_hex(message[0:next_message_index])}})
+    if next_message_index is not None:
+        return assign_command_type(message[next_message_index:], res)
+    else:
+        return res
 
 
-def parse_message(msg: bin, res=None) -> list:
+def get_tlvs(msg: bin, next_index=0, res=None) -> (int, list):
     """
     Parse the message and return a list of TLVs.
+    :param next_index: the index of the next message
     :param msg: binary TLV data
     :param res: list of TLVs. Initialized to none
     :return: list of all TlVs in the message
     """
     # The message can have several TLV's and we'll return a list of all TLV's in the message.
+    type_loc = 0
+    length_loc = 1
     if res is None:
         res = []
     if msg == b'':
-        return res
-    type_loc = 0
-    length_loc = 1
+        return None, res
+    if msg[0:1] == b'\xFF' or msg[0:2] == b'\x00\x15':
+        return next_index, res
     if msg[type_loc] == 0:
         # Source Address TLV
         tlv_type = "Source Address TLV"
@@ -210,36 +182,17 @@ def parse_message(msg: bin, res=None) -> list:
     tlv_length = msg[length_loc]
     tlv_value = msg[length_loc + 1:2 + tlv_length]
     res.append({"type": tlv_type, "length": tlv_length, "value": tlv_value})
-    return parse_message(msg[tlv_length + 2:], res)
 
+    next_index += tlv_length + 2
 
-def print_parsed_message(file: pathlib.Path, msg: dict) -> None:
-    """Just a simple print utility."""
-    print(f"{file.name[:-4].capitalize()}:")
-    for _msg in msg:
-        if _msg.get("destination") == child_addr:
-            dest = "Child"
-        elif _msg.get("destination") == master_addr:
-            dest = "Master"
-        elif _msg.get("destination") == multi_cast:
-            dest = "Multicast"
-        else:
-            dest = _msg.get("destination")
-        print("\t{}: {} - Send to {}".format(_msg.get("count"), _msg.get("command_type").get("name"), dest))
-        for tlv in _msg.get("tlvs"):
-            print("\t\t{}: {}".format(tlv.get("type"), print_hex(tlv.get("value"))))
-        # Print the complete message, as binary
-        print("\t" + print_hex(_msg.get("message")))
+    return get_tlvs(msg[tlv_length + 2:], next_index, res)
 
 
 if __name__ == '__main__':
-    #child_file = pathlib.Path("child.bin")
-    #master_file = pathlib.Path("master.bin")
     bb_file = pathlib.Path("test.bin")
 
-    #child_msg = assign_command_type(handle_messages(read_file(child_file), child_determiner))
-    #master_msg = assign_command_type(handle_messages(read_file(master_file), master_determiner))
-    bb_msg = assign_command_type(handle_messages(read_file(bb_file), master_determiner))
-    #print_parsed_message(child_file, child_msg)
-    #print_parsed_message(master_file, master_msg)
-    print_parsed_message(bb_file, bb_msg)
+    bb_msg = assign_command_type(read_file(bb_file))
+    for msg in bb_msg:
+        print(msg["name"])
+        print(msg["total_message"])
+        print()
